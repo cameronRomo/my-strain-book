759a07174c0e976603e487ca3ac980e6636c9c6f
Cameron Romo <cromo1225@gmail.com>
2021-01-18 02:28:24 -0700

    Refactor to separate components

--- src/App/App.js
+++ src/App/App.js
@@ -1,21 +1,22 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import { NavBar } from '../NavBar/NavBar';
 import { Strains } from '../Strains/Strains';
 import { Experiences } from '../Experiences/Experiences';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
 export const App = () => {
+  const [experiencesLog, setExperiences] = useState([]);
   return (
     <section>
       <NavBar />
       <Switch>
-        <Redirect exact path="/" to="/strains" component={Strains} />
+        <Redirect exact path="/" to="/strains" />
         <Route exact path="/strains">
-          <Strains />
+          <Strains setExperiences={setExperiences} experiencesLog={experiencesLog}/>
         </Route>
         <Route path="/experiences">
-          <Experiences />
+          <Experiences experiencesLog={experiencesLog}/>
         </Route>
       </Switch>
     </section>
