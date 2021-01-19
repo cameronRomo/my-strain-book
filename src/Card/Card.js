b914a857ae39650f734d7b5148dd2e61af7eb316
Cameron Romo <cromo1225@gmail.com>
2021-01-18 18:50:45 -0700

    Add uniqe keys for .maps

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -2,44 +2,34 @@ import React from 'react';
 import './Card.css';
 import leaf from '../leaf.svg';
 import medicalLeaf from '../medicalLeaf.svg';
+import PropTypes from 'prop-types';
+
 export const Card = (props) => {
-  // const [favoritedStrain, setFavorite] = useState(false);
-  // const [experiencesLog, setExperiences] = useState([]);
-
-  // const addFavorite = (event) => {
-  //   console.log(event.target.id);
-  //   console.log(props.strain.id);
-  //   if (props.strain.id) {
-  //     console.log('yay!');
-  //     setFavorite(!favoritedStrain);
-  //     setExperiences([...experiencesLog, props.strain]);
-  //   } 
-  // }
-
+  console.log(props);
   return (
     <section 
-    className='card'
+      className='card'
     >
       <div className='strain-container'>
         <h3 className='selected-strain'>Strain: { props.strainName }</h3>
         <h3 className='selected-type'>{props.strain.race}</h3>
         <div className='strain-flavors'>
           <h3>Flavors:</h3>
-          {props.strain.flavors.map(flavor => (<p>{flavor}</p>) )}
+          {props.strain.flavors.map(flavor => (<p key={flavor}>{flavor}</p>) )}
         </div>
         <h3 className='header-effects'>Effects:</h3>
         <div className='container-effects'>
           <div className='positive-effects'>
             <h3>Positive:</h3>
-            {props.strain.effects.positive.map(flavor => (<p>{flavor}</p>)) }    
+            {props.strain.effects.positive.map(experience => (<p key={experience}>{experience}</p>)) }    
           </div>
           <div className='negative-effects'>
             <h3>Negative:</h3>
-            {props.strain.effects.negative.map(flavor => (<p>{flavor}</p>)) }
+            {props.strain.effects.negative.map(experience => (<p key={experience}>{experience}</p>)) }
           </div>
           <div>
             <h3 className='medical-effects'>Medical:</h3>
-            {props.strain.effects.medical.map(flavor => (<ul>{flavor}</ul>) )}
+            {props.strain.effects.medical.map(experience => (<ul key={experience}>{experience}</ul>) )}
           </div>
         </div>
         {!props.favoritedStrain && 
@@ -56,4 +46,13 @@ export const Card = (props) => {
       </div>
     </section>
   )
+}
+
+Card.propTypes = {
+  id: PropTypes.number,
+  key: PropTypes.number,
+  strainName: PropTypes.string,
+  strain: PropTypes.object,
+  favoritedStrain: PropTypes.bool,
+  addFavorite: PropTypes.func
 }
